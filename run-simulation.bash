c52b1b2e3f2be573b6eebfe5022aa5a07131b67a
user name <at-eidfstaff@eidf114-aturner.vms.os.eidf.epcc.ed.ac.uk>
2024-12-18 19:30:50 +0000

    Add mkdir command to run script

--- run-simulation.bash
+++ run-simulation.bash
@@ -5,6 +5,15 @@ model=$1
 # Cleanup
 # rm -f $PWD/$model/data/output/*.csv
 
+# Create directory if we need to
+outdir=${model}/data/output
+if [[ ! -e $outdir ]]; then
+    echo "Creating: $outdir"
+    mkdir -p $outdir
+elif [[ ! -d $dir ]]; then
+    echo "$outdir already exists but is not a directory" 1>&2
+fi
+
 docker run -v $PWD/$model/data:/data -v $PWD/algorithm:/algorithm --name $model --detach elastisim /data/input/configuration.json --log=root.thresh:warning 
 sleep 20
 docker container ls
