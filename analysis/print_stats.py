ffb1d9afc6806e302a91abba48642c01cd24a124
Andy Turner <a.turner@epcc.ed.ac.uk>
2025-01-09 18:59:24 +0000

    Overall statistics

--- analysis/print_stats.py
+++ analysis/print_stats.py
@@ -17,4 +17,9 @@ stats_df = pd.DataFrame(statsList)
 stats_df = stats_df.transpose()
 stats_df.columns = nameList
 
+stats_df = stats_df.apply(pd.to_numeric)
+
+
 print(stats_df.to_markdown(floatfmt=".3f"))
+
+stats_df.to_csv('stats.csv', float_format='%.3f')
